2ad0372c4d868be7524cb971c66c8be89abc7cd3
Gifaro <figafran@gmail.com>
2016-05-08 00:14:32 -0300

    Te levanta los paramteros del archivo

--- src/modulos/levanta_datos.cpp
+++ src/modulos/levanta_datos.cpp
@@ -1,22 +1,78 @@
 #include "levanta_datos.h"
 
-#define TRAINING_FILE_PATH "data/train.csv"
-#define TRAINING_FILE_SIZE 40000
+#define INPUT_FILE_PATH "tests/test1.in"
 
 LevantaDatos::LevantaDatos(){
+	// Levantar inputs
 	string line;
-	ifstream training_file(TRAINING_FILE_PATH);
+	ifstream input_file(INPUT_FILE_PATH);
 
-	int i = 0;
-	getline (training_file,line);
-	if (training_file.is_open()){
-		while ( getline (training_file,line) && i < 2){
-			cout << line[0] << endl;
-			i++;
+	if (input_file.is_open()){
+		stringstream iss;
+		string token;
+
+		getline(input_file, line);
+		iss << line;
+		getline(iss, token,' ');
+		this->file_path = token + "train.csv";
+		getline(iss, token,' ');
+		this->cantidad_vecinos = (unsigned int) stoi(token);
+		getline(iss, token,' ');
+		this->alpha = (unsigned int) stoi(token);
+		getline(iss, token,' ');
+		this->gamma = (unsigned int) stoi(token);
+		getline(iss, token);
+		this->cantidad_folds = (unsigned int) stoi(token);
+
+		iss.clear();
+
+
+		while ( getline (input_file,line) ){
+			vector<unsigned int> fold;
+
+			iss << line;
+			while ( getline (iss, token, ' ') ){
+				fold.push_back(stoi(token));
+			}
+			this->folds.push_back(fold);
+
+			iss.clear();
 		}
 
-		training_file.close();
+		input_file.close();
 	} else {
-		cout << "no abre nada" << endl;
+		cout << "No se abre el input" << endl;
 	}
+
+	// Levantar d´igitos
+
+	string linea;
+	ifstream data_file(this->FilePath());
+
+	if (data_file.is_open()){
+		getline(data_file, linea);
+
+		data_file.close();
+	} else {
+		cout << "No se abre la data" << endl;
+	}
+
+}
+
+string LevantaDatos::FilePath() const{
+	return this->file_path;
+}
+
+unsigned int LevantaDatos::CantidadVecinos() const{
+	return this->cantidad_vecinos;
+}
+
+unsigned int LevantaDatos::Alpha() const{
+	return this->alpha;
+}
+unsigned int LevantaDatos::Gamma() const{
+	return this->gamma;
+}
+unsigned int LevantaDatos::CantidadFolds() const{
+	return this->cantidad_folds;
 }
--- src/modulos/levanta_datos.h
+++ src/modulos/levanta_datos.h
@@ -4,18 +4,34 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
-
-#include "digito.h"
 
 using namespace std;
 
 class LevantaDatos{
 	private:
-		vector<Digito> training_values;
+		string file_path;
+		unsigned int cantidad_vecinos;
+		unsigned int alpha;
+		unsigned int gamma;
+		unsigned int cantidad_folds;
+
+		vector<double> digitos;
+		vector< char * > labels;
+
+		vector< vector< unsigned int > > folds; // vector de 1 y 0 que me dice si el i-esimo valor es de training (1) o si es de testing (1)
+
 
 	public:
 		LevantaDatos();
+		string FilePath() const;
+		unsigned int CantidadVecinos() const;
+		unsigned int Alpha() const;
+		unsigned int Gamma() const;
+		unsigned int CantidadFolds() const;
+
+
 };
 
 #endif
