56b4ced6cc0bbb6a74b31faf60cd4aa1ad08729e
nacho692 <imariotti@dc.uba.ar>
2016-05-08 13:34:21 -0300

    PLS-DA, faltan un par de metodos

--- src/modulos/identificador.cpp
+++ src/modulos/identificador.cpp
@@ -1,5 +1,62 @@
 
 #include "identificador.h"
+void Identificador::PLS_DA(const Matriz& set, unsigned int gamma){
+	Matriz X,Xt;
+	CentrarDividir(set,X,Xt);
+
+	Matriz Yt;
+	Matriz Y(10,set.Alto());
+
+	for(unsigned int i = 0; i < Y.Alto(); i++){
+		for(unsigned int j = 0; j < Y.Ancho(); j++){
+			Y[i][j] = -1;
+		}
+		Y[i][clases[i]] = 1;
+	}
+
+	Yt = Y;
+	Yt.Transponer();
+
+	Matriz XC = X;
+	Vt = Matriz(set.Ancho(),gamma);
+	for(unsigned int i = 0; i < gamma; i++){
+		Matriz M = Xt*Y*Yt*X;
+		//TODO: Hacerlo random
+		Vector w = Vector(M.Ancho());
+		M.MetodoPotenciaNIteraciones(w,5);
+		w = w * (1/w.Norma());
+
+		Vector t = X*w;
+		t = t * (1/t.Norma());
+		//TODO: Vector por vector
+		//X = X + (-1)*(t*(t*X));
+		//Y = Y + (-1)*(t*(t*Y));
+		Vt[i] = w;
+	}
+
+	//Tengo mi transformacion
+	//Actualizo el training set
+	tSet = Vt*XC;
+}
+
+void Identificador::CentrarDividir(const Matriz& set, Matriz& X, Matriz& Xt) const{
+	//Esto es igual en ambos metodos
+	Xt = set;
+	Xt.Transponer();
+	double escalar = 1/sqrt(set.Alto());
+
+	for(unsigned int i = 0; i < Xt.Alto(); i++){
+		//TODO: Vector + escalar
+		double media = Xt[i].Media();
+		for(unsigned int j = 0; j < Xt.Ancho(); j++){
+			Xt[i][j] -= media;
+			Xt[i][j] *= escalar;
+		}
+	}
+
+	X = Xt;
+	X.Transponer();
+}
 
 const Matriz& Identificador::cambioBase() const{
 	return Vt;
@@ -9,13 +66,14 @@ const Matriz& Identificador::trainingSet() const{
 	return tSet;
 }
 
-string Identificador::kNN(const Vector& v) const{
+int Identificador::kNN(const Vector& v) const{
 	//TODO: Cambio de base con V
+	Vector vb = Vt*v;
 
 	vector<Vecino> distancias;
 	for(unsigned int i = 0; i < tSet.Alto(); i++){
 		Vecino d;
-		d.distancia = v.Distancia(tSet[i]);
+		d.distancia = vb.Distancia(tSet[i]);
 		d.clase = clases[i];
 		distancias.push_back(d);
 	}
@@ -23,14 +81,14 @@ string Identificador::kNN(const Vector& v) const{
 	make_heap(distancias.begin(),distancias.end(),cmpVecinoDistancia);
 
 	vector<Vecino> vecinos;
-	for(unsigned int i = 0; i < kVecinos; i++){
+	for(unsigned int i = 0; i < cantidad_vecinos; i++){
 		pop_heap(distancias.begin(), distancias.end(),cmpVecinoDistancia);
 		vecinos.push_back(distancias[distancias.size()-1]);
 		distancias.pop_back();
 	}
 
 	//Por si ya lo teniamos cargado en el training set basicamente
-	if(vecinos[0].distancia == 0){
+	if(dEq(vecinos[0].distancia)){
 		return vecinos[0].clase;
 	}
 
@@ -38,8 +96,8 @@ string Identificador::kNN(const Vector& v) const{
 	sort(vecinos.begin(),vecinos.end(),cmpVecinoClase);
 	double max = 0;
 	double sum = 0;
-	string claseMax;
-	string clase = vecinos[0].clase;
+	int claseMax;
+	int clase = vecinos[0].clase;
 	for(unsigned int i = 0; i < vecinos.size(); i++){
 		//Si llegue al final o si cambio de clase, me fijo si supera al maximo anterior
 		if(clase != vecinos[i].clase || i == vecinos.size()-1){
--- src/modulos/identificador.h
+++ src/modulos/identificador.h
@@ -3,22 +3,23 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <cmath>
 #include "../utils/matriz.h"
 
 class Identificador{
 	private:
 		unsigned int alpha;
-		unsigned int kVecinos;
+		unsigned int cantidad_vecinos;
 		unsigned int gamma;
 		
 		Matriz tSet;
-		vector<string> clases;
+		vector<int> clases;
 
 		Matriz Vt;
 
 		struct Vecino{
 			double distancia;
-			string clase;
+			int clase;
 		};
 
 		static bool cmpVecinoDistancia(const Vecino& v1,const Vecino& v2){
@@ -28,13 +29,16 @@ class Identificador{
 		static bool cmpVecinoClase(const Vecino& v1,const Vecino& v2){
 			return v1.clase > v2.clase;
 		}
+		static bool dEq(double d1, double d2 = 0, double e = 0.0000001){
+			return abs(d1-d2) < e;
+		}
 
 		//Tienen que modificar tSet y Vt ambas
 		void PCA(const Matriz& set, unsigned int alpha);
 		void PLS_DA(const Matriz& set, unsigned int gammma);
-
+		void CentrarDividir(const Matriz& set, Matriz& X, Matriz& Xt) const;
 	public:
-		string kNN(const Vector& v) const;
+		int kNN(const Vector& v) const;
 		const Matriz& trainingSet() const;
 		const Matriz& cambioBase() const;
 };
