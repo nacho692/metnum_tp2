ea97123979547b4e643fe52e7cc6aaa0b7e357db
Gifaro <francisco.figari@hotmail.com>
2016-05-09 22:01:58 -0300

    escupe datos

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include "modulos/identificador.h"
+#include "modulos/escupe_datos.h"
 #include "modulos/levanta_datos.h"
-int main(){
-	LevantaDatos ld;
-	ld.SetearKesimoFold(0);
+int main(int argc, char const *argv[]){
+	std::string nombre_entrada(argv[1]);
+	std::string nombre_salida(argv[2]);
+	std::string metodo_a_utilizar(argv[3]);		//(0: kNN , 1: PCA + kNN ,2: PLS-DA + kNN)
 
-	Matriz mt = ld.MatrizTesting();
-	Identificador id(ld.LabelsTesting(),ld.CantidadVecinos());
-	id.PLS_DA(mt,1);
-	/*for(unsigned int i = 0; i < 10; i++){
-		cout << ld.LabelsTesting()[i] << endl;
-		cout << id.kNN(ld.MatrizTesting()[i]) << endl;
-	}*/
+	// LevantaDatos ld(nombre_entrada);
+	// ld.SetearKesimoFold(0);
+
+	// Matriz mt = ld.MatrizTraining();
+	// Identificador id(ld.LabelsTraining(),ld.CantidadVecinos());
+
+	// id.PCA(mt, ld.Alpha())
+	// for(unsigned int i = 0; i < 10; i++){
+	// 	cout << ld.LabelsTesting()[i] << endl;
+	// 	cout << id.kNN(ld.MatrizTesting()[i]) << endl;
+	// }
+
+	EscupeDatos ep(nombre_salida);
+
 	return 0;
 }
---
+++ src/modulos/escupe_datos.h
@@ -0,0 +1,19 @@
+#ifndef ESCUPE_DATOS_H_
+#define ESCUPE_DATOS_H_
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class EscupeDatos{
+	// private:
+
+
+	public:
+		EscupeDatos(string output_file_name);
+
+};
+
+#endif
