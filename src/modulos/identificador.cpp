22127fbb7259fe45df51d90137c16199826a8b4f
ignacio mariotti <mariotti.ignacio@gmail.com>
2016-05-11 15:14:11 -0300

    countsort ahora que tenemos int

--- src/modulos/identificador.cpp
+++ src/modulos/identificador.cpp
@@ -151,27 +151,19 @@ int Identificador::kNN(const Vector& v) const{
 	}
 
 	//Ordenamos para juntar cada clase y asi sacar el voto calificado mas facil (no tengo acentos)
-	sort(vecinos.begin(),vecinos.end(),cmpVecinoClase);
-	double max = 0;
-	double sum = 0;
-	int claseMax;
-	int clase = vecinos[0].clase;
+	vector<double> votos(10);
 	for(unsigned int i = 0; i < vecinos.size(); i++){
-		//Si llegue al final o si cambio de clase, me fijo si supera al maximo anterior
-		if(clase != vecinos[i].clase || i == vecinos.size()-1){
-			//Sumo el ultimo si es la ultima vuelta
-			sum += (1/vecinos[i].distancia)*(i== vecinos.size()-1); 
-			if(sum > max){
-				max = sum;
-				claseMax = clase;
-			}
-			sum = 0;
-			clase = vecinos[i].clase;
-		}
-		sum += (1/vecinos[i].distancia);
+		votos[vecinos[i].clase] += 1/vecinos[i].distancia;
 	}
-
-	return claseMax;
+	unsigned int maxD = 0;
+	double maxV = 0;
+	for(unsigned int i = 0; i < votos.size(); i++){
+		if(votos[i] > maxV){
+			maxD = i;
+			maxV = maxD;
+		}
+	}
+	return maxD;
 }
 /*
 unsigned int Identificador::Alpha()const{
